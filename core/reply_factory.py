44a3c3bceec9e029c04512ebce9538212a0ba353
Yadhukrishnan <yadhukrishnanvc7@gmail.com>
2023-08-01 18:37:45 +0530

    record_current_answer , get_next_question , generate_final_response Changes

--- core/reply_factory.py
+++ core/reply_factory.py
@@ -8,11 +8,11 @@ def generate_bot_responses(message, session):
     current_question_id = session.get("current_question_id")
     if not current_question_id:
         bot_responses.append(BOT_WELCOME_MESSAGE)
+    else:
+        success, error = record_current_answer(message, current_question_id, session)
 
-    success, error = record_current_answer(message, current_question_id)
-
-    if not success:
-        return [error]
+        if not success:
+            return [error]
 
     next_question, next_question_id = get_next_question(current_question_id)
 
@@ -28,10 +28,15 @@ def generate_bot_responses(message, session):
     return bot_responses
 
 
-def record_current_answer(answer, current_question_id):
+def record_current_answer(answer, current_question_id, session):
     '''
     Validates and stores the answer for the current question to session.
     '''
+    if current_question_id != None:
+        question = PYTHON_QUESTION_LIST[current_question_id - 1] if current_question_id > 0 else None
+        if question and question["answer"] == answer:
+            session["correct_answers"] = session.get("correct_answers", 0) + 1
+
     return True, ""
 
 
@@ -39,14 +44,23 @@ def get_next_question(current_question_id):
     '''
     Fetches the next question from the PYTHON_QUESTION_LIST based on the current_question_id.
     '''
-
-    return "dummy question", -1
-
+    if current_question_id == None:
+        current_question_id=0
+        
+    if current_question_id < len(PYTHON_QUESTION_LIST):
+        next_question = PYTHON_QUESTION_LIST[current_question_id]
+        question_text = str(next_question["question_text"])
+        options_text = str(next_question["options"])
+        return f"{question_text} \n Options: \n {options_text}", current_question_id + 1
+    else:
+        return None, None
 
 def generate_final_response(session):
     '''
     Creates a final result message including a score based on the answers
     by the user for questions in the PYTHON_QUESTION_LIST.
     '''
-
-    return "dummy result"
+    correct_answers = session.get("correct_answers", 0)
+    if correct_answers != 0:
+        del session['correct_answers']
+    return f"You answered {correct_answers} out of {len(PYTHON_QUESTION_LIST)} questions correctly!"
